6ee17d4b6133f5f91aebdd46fd4dfc3fa09f373e
Nipun Agrawal <nipunagrawal500@gmail.com>
2025-09-13 00:28:30 +0530

    created basic theme and theme switcher

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { ThemeToggle } from '../components/ThemeToggle'
 
 const HomeScreen = () => {
   return (
-    <div className = "min-h-screen">
-      heheehe
+    <div className = "min-h-screen bg-background text-foreground">
+
+      <ThemeToggle/>
+
+
+
+
+
+
+
     </div>
   )
 }
